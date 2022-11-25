809538e74014b7ff041e88f0873484bfef8360ad
vinayyadav3016 <vinayyadav3016@gmail.com>
2022-11-25 23:28:14 +0530

    Nvim settings

--- .config/nvim/nvim-cmp.lua
+++ .config/nvim/nvim-cmp.lua
@@ -51,8 +51,11 @@ sources = cmp.config.sources({
 })
 
 -- Setup lspconfig.
--- local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- require('lspconfig')['pyright'].setup {
+-- capabilities = capabilities
+-- }
+-- require('lspconfig')['clangd'].setup {
 -- capabilities = capabilities
 -- }
