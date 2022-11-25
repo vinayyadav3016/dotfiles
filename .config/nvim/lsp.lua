70e1dc18bbd853c1814174342cbc91143f00d5ff
vinayyadav3016 <vinayyadav3016@gmail.com>
2022-11-25 23:27:50 +0530

    Disbale nvim-lsp

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -1,53 +1,8 @@
 --require'lspconfig'.pyright.setup{}
 --require'lspconfig'.clangd.setup{}
 require'lspconfig'.gdscript.setup{}
-
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '[o', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']l', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[w', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']w', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>dc', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>df', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ho', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>im', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>sg', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>td', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rf', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-end
-
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
--- local servers = { 'pyright', 'rust_analyzer', 'clangd', 'gdscript' }
-local servers = { 'gdscript' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
-end
-
+--------------------------------------------------------------------------------
+-- useful functions
 local function setup_lsp_diags()
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics,
@@ -79,9 +34,48 @@ local function disable_inline_diags()
     severity_sort = false,
   })
 end
-
-vim.api.nvim_set_keymap('n', '[l', '<cmd>lua vim.diagnostic.config({virtual_text = true})<CR>', opts)
-vim.api.nvim_set_keymap('n', ']l', '<cmd>lua vim.diagnostic.config({virtual_text = false})<CR>', opts)
---vim.api.nvim_set_keymap('n', ']l', '<cmd>lua vim.lsp.disable_inline_diags()<CR>', opts)
---setup_lsp_diags()
+--------------------------------------------------------------------------------
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+-- vim.api.nvim_set_keymap('n', '[o', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+-- vim.api.nvim_set_keymap('n', ']l', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+-- vim.api.nvim_set_keymap('n', '[v', '<cmd>lua vim.diagnostic.config({virtual_text = true})<CR>', opts)
+-- vim.api.nvim_set_keymap('n', ']v', '<cmd>lua vim.diagnostic.config({virtual_text = false})<CR>', opts)
+-- vim.api.nvim_set_keymap('n', '[w', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+-- vim.api.nvim_set_keymap('n', ']w', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+--------------------------------------------------------------------------------
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[h', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']q', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']s', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']t', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+--  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+--------------------------------------------------------------------------------
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+-- local servers = { 'pyright', 'rust_analyzer', 'clangd', 'gdscript' }
+local servers = { }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
+end
+--------------------------------------------------------------------------------
 disable_inline_diags()
