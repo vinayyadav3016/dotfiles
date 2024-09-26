8dcea7103cf9d9d8f00700bf2ce0cc0b6d9f4ba1
vinayyadav3016 <vinayyadav3016@gmail.com>
2024-09-26 12:18:23 +0530

    Preparing to merge

--- syncDotfiles.sh
+++ syncDotfiles.sh
@@ -3,7 +3,8 @@
 DEVICE=JOKER
 ############################## Settings  #######################################
 BACKUPFOLDER=/home/vinay/SharedData/Backup/Devices/${DEVICE}
-BACKUPFILE=${BACKUPFOLDER}/${DEVICE}_home_dotfile_`date +%Y-%m-%d`.tar.gz
+#BACKUPFILE=${BACKUPFOLDER}/${DEVICE}_home_dotfile_`date +%Y-%m-%d`.tar.gz
+BACKUPFILE=${BACKUPFOLDER}/${DEVICE}_home_dotfile_`date +%Y-%m-%d`.tar.xz
 BACKUPFILELIST=${BACKUPFOLDER}/listoffiles.txt
 EXCLUDEFILE=${BACKUPFOLDER}/excludefolders.txt
 CONFIGEXCLUDEFILE=${BACKUPFOLDER}/configexcludefolders.txt
@@ -15,12 +16,12 @@ echo 'Backing up ' `wc -l ${BACKUPFILELIST} | cut -d ' ' -f 1` "INODES"
 pacman -Qqe > ${BACKUPFOLDER}/native_package.txt
 pacman -Qm  > ${BACKUPFOLDER}/mannual_package.txt
 ##################### Back them up #############################################
-tar -I pigz -cf ${BACKUPFILE} -T ${BACKUPFILELIST} ${BACKUPFOLDER}/native_package.txt ${BACKUPFOLDER}/mannual_package.txt
+tar -I 'xz -9 -T4' -cf ${BACKUPFILE} -T ${BACKUPFILELIST} ${BACKUPFOLDER}/native_package.txt ${BACKUPFOLDER}/mannual_package.txt
 ##################### Back them up #############################################
-BACKUPFILE=${BACKUPFOLDER}/${DEVICE}_etc_`date +%Y-%m-%d`.tar.gz
-sudo tar -I pigz -cf ${BACKUPFILE} /etc
-BACKUPFILE=${BACKUPFOLDER}/${DEVICE}_boot_`date +%Y-%m-%d`.tar.gz
-sudo tar -I pigz -cf ${BACKUPFILE} /boot
+BACKUPFILE=${BACKUPFOLDER}/${DEVICE}_etc_`date +%Y-%m-%d`.tar.xz
+sudo tar -I 'xz -9 -T4' -cf ${BACKUPFILE} /etc
+BACKUPFILE=${BACKUPFOLDER}/${DEVICE}_boot_`date +%Y-%m-%d`.tar.xz
+sudo tar -I 'xz -9 -T4' -cf ${BACKUPFILE} /boot
 ############################ Remove file #######################################
 rm ${BACKUPFILELIST}
 rm ${BACKUPFOLDER}/native_package.txt
