2840a297948195603e38b818e3038fd9a07958a4
vinayyadav3016 <vinayyadav3016@gmail.com>
2025-05-22 22:59:18 +0530

    Added  preview and disable auto pasting

--- nvim/leap.lua
+++ nvim/leap.lua
@@ -12,6 +12,12 @@ require("leap").set_default_mappings()
 require("leap").opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
 require("leap").opts.case_senstive = true
 require("leap.user").set_repeat_keys("<enter>", "<backspace>")
+-- Skip the middle of alphabetic words:
+--   foobar[quux]
+--   ^----^^^--^^
+require("leap").opts.preview_filter = function(ch0, ch1, ch2)
+    return not (ch1:match("%s") or ch0:match("%a") and ch1:match("%a") and ch2:match("%a"))
+end
 --------------------------------------------------------------------------------
 vim.api.nvim_create_augroup("LeapHighlights", { clear = true })
 vim.api.nvim_create_autocmd({ "Colorscheme" }, {
@@ -40,4 +46,15 @@ vim.api.nvim_create_autocmd({ "Colorscheme" }, {
     end,
 })
 vim.api.nvim_exec_autocmds("Colorscheme", { group = "LeapHighlights" })
+--------------------------------------------------------------------------------
+-- vim.api.nvim_create_autocmd("User", {
+    -- pattern = "RemoteOperationDone",
+    -- group = vim.api.nvim_create_augroup("LeapRemote", {}),
+    -- callback = function(event)
+        -- -- Do not paste if some special register was in use.
+        -- if vim.v.operator == "y" and event.data.register == '"' then
+            -- vim.cmd("normal! p")
+        -- end
+    -- end,
+-- })
 --------------------------------------------------------------------------------
