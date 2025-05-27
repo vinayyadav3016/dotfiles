6dd53c9e5226dc7194add267833330e071f97d09
vinayyadav3016 <vinayyadav3016@gmail.com>
2025-05-27 09:26:45 +0530

    Install fzf

--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -1,14 +1,24 @@
 --------------------------------------------------------------------------------
 -- telescope links
-local actions = require('telescope.actions')
-require('telescope').setup {
+local actions = require("telescope.actions")
+require("telescope").setup({
     defaults = {
         mappings = {
             n = {
                 ["q"] = actions.close,
-                ['<C-d>'] = actions.delete_buffer,
+                ["<C-d>"] = actions.delete_buffer,
             },
-       },
-    }
-}
+        },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        },
+    },
+})
+require('telescope').load_extension('fzf')
 --------------------------------------------------------------------------------
