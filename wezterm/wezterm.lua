f85224b00497217afd3a45f71c6abb1cddb9d34f
vinayyadav3016 <vinayyadav3016@gmail.com>
2025-07-18 23:11:21 +0530

    Added shortcuts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -65,5 +65,18 @@ wezterm.on(
   end
 )
 
+config.keys = {
+    {key="LeftArrow", mods="CMD", action=wezterm.action{ActivateTabRelative=-1}},
+    {key="RightArrow", mods="CMD", action=wezterm.action{ActivateTabRelative=1}},
+    {key="1", mods="OPT", action=wezterm.action.ActivateTab(0)},
+    {key="2", mods="OPT", action=wezterm.action.ActivateTab(1)},
+    {key="3", mods="OPT", action=wezterm.action.ActivateTab(2)},
+    {key="4", mods="OPT", action=wezterm.action.ActivateTab(3)},
+    {key="5", mods="OPT", action=wezterm.action.ActivateTab(4)},
+    {key="6", mods="OPT", action=wezterm.action.ActivateTab(5)},
+    {key="7", mods="OPT", action=wezterm.action.ActivateTab(6)},
+    {key="8", mods="OPT", action=wezterm.action.ActivateTab(7)},
+    {key="9", mods="OPT", action=wezterm.action.ActivateTab(8)},
+}
 -- Finally, return the configuration to wezterm:
 return config
